8162fe4488dc7bcc21820d70410f46bdc539e22e
Benjamin Peterson <benjamin@python.org>
2016-04-16 13:31:48 -0700

    open ssllabs links in another tab

--- check_https.py
+++ check_https.py
@@ -166,13 +166,13 @@ def check_secure_connection(info):
         secure_sock.close()
     msg = "A verified TLS connection can be established. "
     if info.ssllabs_grade is not None:
-        grade_msg = "<a href=\"https://www.ssllabs.com/ssltest/analyze.html?d={}\">SSL Labs grade</a> is " + info.ssllabs_grade + "."
+        grade_msg = "<a href=\"https://www.ssllabs.com/ssltest/analyze.html?d={}\" target=\"_blank\">SSL Labs grade</a> is " + info.ssllabs_grade + "."
         if info.ssllabs_grade == "F":
             good_connection.fail(grade_msg.format(info.domain))
             return
         msg += grade_msg
     else:
-        msg += "(<a href=\"https://www.ssllabs.com/ssltest/analyze.html?d={}\">SSL Labs report</a>)"
+        msg += "(<a href=\"https://www.ssllabs.com/ssltest/analyze.html?d={}\" target=\"_blank\">SSL Labs report</a>)"
     info.secure_connection_works = True
     good_connection.succeed(msg.format(info.domain))
 
